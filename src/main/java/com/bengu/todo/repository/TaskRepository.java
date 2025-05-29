e6d498b9017df874292ba2135af64f87d2a04b88
bengu <bnguyilmaz@gmail.com>
2025-05-29 13:40:37 +0300

    functions are added in takerepository interface

--- src/main/java/com/bengu/todo/repository/TaskRepository.java
+++ src/main/java/com/bengu/todo/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package main.java.com.bengu.todo.repository;
 
+import main.java.com.bengu.todo.model.User;
+import java.util.List;
+
+
 public interface TaskRepository {
+    List<User> loadUsers();
+    void saveUsers(List<User> users);
 }
